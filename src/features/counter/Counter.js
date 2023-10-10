592daa65b2c79fc56be547657167e1250a11d828
nikhilsinha822 <113183657+nikhilsinha822@users.noreply.github.com>
2023-10-10 23:20:18 +0530

    cirrected reset button

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -3,21 +3,21 @@ import { useSelector, useDispatch } from 'react-redux'
 import { increment, decrement, reset, incrementByValue } from './counterSlice'
 
 const Counter = () => {
-const [value, setValue] = useState(0);
-const number = Number(value) || 0;
+const [incValue, setIncValue] = useState(0);
+const number = Number(incValue) || 0;
 const count = useSelector((state)=> state.counter.count);
 const dispatch = useDispatch();
 const resetAll = () => {
-    setValue(0);
+    setIncValue(0);
     dispatch(reset());
 }
   return <section>
     {count}
     <button onClick={()=> dispatch(increment())}>+</button>
     <button onClick={()=> dispatch(decrement())}>-</button>
-    <input type="text" onChange={(e)=>setValue(e.target.value)}/>
+    <input type="text" onChange={(e)=>setIncValue(e.target.value)} value={incValue}/>
     <button onClick={()=> dispatch(incrementByValue(number))}>Add Value</button>
-    <button onClick={()=>resetAll}>Reset</button>
+    <button onClick={resetAll}>Reset</button>
   </section>
 }
 
